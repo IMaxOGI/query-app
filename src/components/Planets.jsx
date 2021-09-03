543e473244413c78da6394993ea6e71c401babe9
IMaxOGI <max165773@gmail.com>
2021-09-03 19:14:46 +0300

    update

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React from "react";
 import { useQuery } from "react-query";
 import Planet from "./Planet";
 
@@ -9,16 +9,11 @@ const fetchPlanets = async (key) => {
 };
 
 const Planets = () => {
-  const [page, setPage] = useState(2);
   const { resolveData, status } = useQuery(["planets"], fetchPlanets);
 
   return (
     <div>
       <h2>Planets</h2>
-
-      <button onClick={() => setPage(1)}>page 1</button>
-      <button onClick={() => setPage(2)}>page 2</button>
-      <button onClick={() => setPage(3)}>page 3</button>
 
       {status === "loading" && <div>Loading data...</div>}
       {status === "error" && <div>Error fetching data</div>}
